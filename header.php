10b2a725e7c16fed2689e6be524a1a4db487f5ae
shoutaoumaru <otokmae.black@gmail.com>
2021-09-25 11:23:50 +0900

    Goolge広告コンバージョンタグマネージャー

--- header.php
+++ header.php
@@ -30,9 +30,14 @@
   <?php wp_head(); ?> 
 </head>
 <body>
+  <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.       googletagmanager.com/ns.html?id=GTM-PDRVQCK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <header id="header">
     <div id="head_container" class="p-md-3 p-2">
-      <a href="/">
+      <a id="top" href="/">
         <img class="" src="<?php echo get_template_directory_uri(); ?>/images/onestopriv.png" alt="" width="220">
       </a>
       <?php if( !wp_is_mobile() ): ?> 
